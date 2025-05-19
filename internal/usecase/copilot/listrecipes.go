38858d40488bdba7dae15eef1fdaf527a10e9b9b
taku-ting <ting.taku@gmail.com>
2025-05-19 16:25:41 +0900

    feat(menu,recipe): sort menuItem.options/recipeGroup.recipes

--- internal/usecase/copilot/listrecipes.go
+++ internal/usecase/copilot/listrecipes.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"hackbar-copilot/internal/domain/recipe"
 	"hackbar-copilot/internal/usecase/sort"
+	"slices"
 )
 
 // ListRecipes implements Copilot.
@@ -13,6 +14,15 @@ func (c *copilot) ListRecipes(sortFunc sort.Yield[recipe.RecipeGroup]) ([]recipe
 		if err != nil {
 			return nil, fmt.Errorf("failed to retrieve recipe groups: %w", err)
 		}
+		slices.SortFunc(rg.Recipes, func(a, b recipe.Recipe) int {
+			if a.Name == b.Name {
+				return 0
+			}
+			if a.Name < b.Name {
+				return -1
+			}
+			return 1
+		})
 		root = sort.Insert(root, rg, sortFunc)
 	}
 	recipeGroups := []recipe.RecipeGroup{}
--- internal/usecase/order/listmenu.go
+++ internal/usecase/order/listmenu.go
@@ -3,16 +3,26 @@ package order
 import (
 	"hackbar-copilot/internal/domain/menu"
 	"hackbar-copilot/internal/usecase/sort"
+	"slices"
 )
 
 // ListMenu implements Order.
 func (o *orderimpl) ListMenu(sortFunc sort.Yield[menu.Item]) ([]menu.Item, error) {
 	var root *sort.Node[menu.Item]
-	for rg, err := range o.datasource.Menu().All() {
+	for item, err := range o.datasource.Menu().All() {
 		if err != nil {
 			return nil, err
 		}
-		root = sort.Insert(root, rg, sortFunc)
+		slices.SortFunc(item.Options, func(a, b menu.ItemOption) int {
+			if a.Name == b.Name {
+				return 0
+			}
+			if a.Name < b.Name {
+				return -1
+			}
+			return 1
+		})
+		root = sort.Insert(root, item, sortFunc)
 	}
 	menuGroups := []menu.Item{}
 	sort.InOrderTraversal(root, &menuGroups)
